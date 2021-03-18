c868c8530fd6606896a3bc8e291e3df6b18d6634
KhairilAzizie <shafiqapianist165@gmail.com>
2021-03-18 19:23:22 +0800

    initial commit

--- samples/src/samples/demo/GitTest.java
+++ samples/src/samples/demo/GitTest.java
@@ -2,7 +2,7 @@ package samples.demo;
 
 public class GitTest {
 	public static void main (String[]args ) {
-		System.out.print("Git is Fun");
+		System.out.print("Git is Fun"+" \nGit is cool");
 	}
 
 }
